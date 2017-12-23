8f2be406eaa7623096fdbe6279c7784a4385ae74
Himanshijaat <puniya844@gmail.com>
2017-12-23 10:31:41 +0530

    pooja paath file working

--- app/src/main/java/com/example/hp/musicapp/HomeFragment.java
+++ app/src/main/java/com/example/hp/musicapp/HomeFragment.java
@@ -12,7 +12,6 @@ import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import java.util.ArrayList;
 import java.util.List;
 
